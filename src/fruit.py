b4e86d71c257091bd867dd2990953a0ccd41d83b
James Hurford <james.hurford@ubiquitypress.com>
2018-01-17 18:48:00 +0000

    Added orange

--- src/fruit.py
+++ src/fruit.py
@@ -1,5 +1,17 @@
 class Fruit(object):
+    _name = 'fruit'
+
     @property
     def name(self):
-        return 'fruit'
+        return self._name
+
+
+class Orange(Fruit):
+    _name = 'Orange'
+
+
+if __name__ == '__main__':
+    fruit = Orange()
+
+    print(fruit.name)
 
